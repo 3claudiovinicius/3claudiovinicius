095170d562d792aef10b2263d64b7c818a892098
Claudio Vinicius Ohara Soares <50547720+3claudiovinicius@users.noreply.github.com>
2023-09-12 09:25:28 +0000

    ok

---
+++ hellowrold.py
@@ -0,0 +1,23 @@
+#def main():
+#    print("Hello, World")
+#    number=input("Type a number")
+#    print("This is your number", number)
+
+#if __name__=="__main__":
+#    main()
+
+#-------------------------------------------
+
+#mylist=[0,"1",8.9,"end"]
+#print(mylist[::-1])
+
+#-------------------------------------------
+
+mystr="teste0"
+def examplefunc():
+    #global mystr
+    mystr=1
+    print(mystr)
+
+examplefunc()
+print(mystr)
